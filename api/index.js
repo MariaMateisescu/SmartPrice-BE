39b0fc17af262ad65b786bd104c2b5a6e5299c9c
Maria Mateisescu <maria.mateisescu@yahoo.com>
2023-06-20 20:49:41 +0300

    vercel 2

---
+++ api/index.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cors = require('cors');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
+const locationRouter = require('./routes/locationRoutes');
+const marketRouter = require('./routes/marketRoutes');
+const categoryRouter = require('./routes/categoryRoutes');
+const recipeRouter = require('./routes/recipeRoutes');
+const shoppingListRouter = require('./routes/shoppingListRoutes');
+const cardsRouter = require('./routes/cardRoutes');
+
+const app = express();
+
+// 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from the body into req.body
+app.use(
+  express.json({
+    limit: '10kb',
+  })
+);
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization againt XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+// Test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.headers);
+  next();
+});
+
+// 3) ROUTES
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
+app.use('/api/locations', locationRouter);
+app.use('/api/markets', marketRouter);
+app.use('/api/categories', categoryRouter);
+app.use('/api/recipes', recipeRouter);
+app.use('/api/shopping-lists', shoppingListRouter);
+app.use('/api/cards', cardsRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;

1a8fb47bd38085ce3cd503299ee6d0a5ba130e88
Maria Mateisescu <maria.mateisescu@yahoo.com>
2023-06-21 14:12:01 +0300

    ready for production

--- app.js
+++ app.js
@@ -20,6 +20,16 @@ const cardsRouter = require('./routes/cardRoutes');
 
 const app = express();
 
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
 // 1) GLOBAL MIDDLEWARES
 // Implement CORS
 app.use(cors());
@@ -92,5 +102,31 @@ app.all('*', (req, res, next) => {
 });
 
 app.use(globalErrorHandler);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    // options object
+  })
+  .then(() => console.log('DB connection successful'));
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
 
 module.exports = app;

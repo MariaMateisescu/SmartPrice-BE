265fbcc1eac98622ea8101d82e15592731db8161
Maria Mateisescu <maria.mateisescu@yahoo.com>
2023-06-29 01:32:05 +0300

    ready for production

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,15 +15,6 @@ const signToken = (id, role, name) => {
 const createSendToken = (user, statusCode, res) => {
   console.log(user.name);
   const token = signToken(user._id, user.role, user.name);
-  const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-  };
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
-
-  res.cookie('jwt', token, cookieOptions);
 
   // Remove password from output
   user.password = undefined;
